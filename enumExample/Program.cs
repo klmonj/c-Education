a4e8594853304911686d0bdd8ebd89e78009eac7
klmonj <klmonj@naver.com>
2021-08-06 19:14:06 +0900

    enum

---
+++ enumExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumExample
+{
+    enum ITEMTYPE //명찰에 가까운값(값형)
+    {
+        EQIP, //장비
+        POTION, //포션
+        QUEST, //퀘스트
+        NONESELECT //선택x
+    }
+    class Item
+    {
+        public ITEMTYPE ItemType = ITEMTYPE.NONESELECT;
+
+        public void PotionTypeSetting()
+        {
+            ItemType = ITEMTYPE.POTION;
+        }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item NewItem = new Item();
+            NewItem.ItemType = ITEMTYPE.POTION;
+            NewItem.PotionTypeSetting();
+
+            Console.WriteLine(ITEMTYPE.POTION);
+            ITEMTYPE Type = ITEMTYPE.POTION;
+            switch (Type)
+            {
+                case ITEMTYPE.EQIP:
+                    break;
+                case ITEMTYPE.POTION:
+                    break;
+                case ITEMTYPE.QUEST:
+                    break;
+                case ITEMTYPE.NONESELECT:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
